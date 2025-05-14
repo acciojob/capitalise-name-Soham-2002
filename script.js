8b66adbb8c56b443bb1457d00256337c3f25814f
Soham Kundu <sohamkundu29012002@gmail.com>
2025-05-14 22:45:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+let ch = document.querySelector("input")
+
+ch.addEventListener("change", (e) =>{
+	let str = e.target.value;
+	let upperCaseString = str.toUpperCase();
+	e.target.value = upperCaseString;
+})
